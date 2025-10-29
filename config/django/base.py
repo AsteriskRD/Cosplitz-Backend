149586d325f0fa760398d8a89fa743ac391dedad
Stephen Omotoso Eniola <107183481+SteveEnny@users.noreply.github.com>
2025-10-29 19:44:43 +0100

    Update email settings for SMTP configuration

--- config/django/base.py
+++ config/django/base.py
@@ -160,9 +160,9 @@ SPECTACULAR_SETTINGS = {
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env('EMAIL_PORT')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'omotosoeniola191@gmail.com'
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')

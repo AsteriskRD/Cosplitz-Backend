c2b01309660abdeeef678c694417d013861b173c
MichaelGbolahan <michaelgbolly@gmail.com>
2025-12-14 21:16:28 +0100

    implement admin forget password API

--- apps/admin_panel/urls.py
+++ apps/admin_panel/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("login/", views.admin_login),
+    path("forget_password/", views.admin_forget_password, name='admin_forget_password'),
+    path("reset_password/", views.admin_reset_password, name='admin_reset_password'),
+
+
     path("kyc/", views.list_kyc, name="admin_list_kyc"),
     path("kyc/<int:kyc_id>/", views.kyc_detail, name="admin_kyc_detail"),
     path("kyc/<int:kyc_id>/approve/", views.approve_kyc, name="admin_approve_kyc"),
--- apps/admin_panel/views.py
+++ apps/admin_panel/views.py
@@ -6,14 +6,24 @@ from rest_framework import status
 from apps.kyc.models import KYCVerification
 from apps.admin_panel.permissions import IsAdminUser
 from django.core.mail import send_mail
-
-
+from django.conf import settings
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+from django.utils.crypto import get_random_string
+from django.utils import timezone
+from datetime import timedelta
+
+from .models import AdminPasswordReset
+from django.contrib.auth import get_user_model
+import random
+
+
+User = get_user_model()
 
 
 @api_view(["POST"])
@@ -40,6 +50,76 @@ def admin_login(request):
         "refresh": str(refresh),
         "access": str(refresh.access_token)
     }, status=200)
+
+
+#forget password
+@api_view(["POST"])
+def admin_forget_password(request):
+    email = request.data.get("email")
+
+    if not email:
+        return Response({"message": "Email is required"}, status=400)
+
+    try:
+        user = User.objects.get(email=email, is_admin=True)
+    except User.DoesNotExist:
+        return Response({"message": "Admin user not found"}, status=404)
+
+    # Generate 6-digit code
+    reset_code = str(random.randint(100000, 999999))
+
+    # Save token
+    AdminPasswordReset.objects.create(
+        user=user,
+        token=reset_code
+    )
+
+    # Send email
+    send_mail(
+        subject="Admin Password Reset Code",
+        message=f"Your password reset code is: {reset_code}",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+        fail_silently=False,
+    )
+
+    return Response({"message": "Password reset code sent"}, status=200)
+
+
+#reset password
+@api_view(["POST"])
+def admin_reset_password(request):
+    email = request.data.get("email")
+    token = request.data.get("token")
+    new_password = request.data.get("password")
+
+    if not all([email, token, new_password]):
+        return Response({"message": "Missing fields"}, status=400)
+
+    try:
+        user = User.objects.get(email=email, is_admin=True)
+    except User.DoesNotExist:
+        return Response({"message": "Admin not found"}, status=404)
+
+    try:
+        reset_entry = AdminPasswordReset.objects.filter(
+            user=user,
+            token=token
+        ).latest("created_at")
+    except AdminPasswordReset.DoesNotExist:
+        return Response({"message": "Invalid reset token"}, status=400)
+
+    # Check expiration (15 minutes recommended)
+    if reset_entry.is_expired():
+        return Response({"message": "Token expired"}, status=400)
+
+    # Update password
+    user.set_password(new_password)
+    user.save()
+
+    return Response({"message": "Password reset successful"}, status=200)
+
+
 
 
 
@@ -115,7 +195,7 @@ def approve_kyc(request, kyc_id):
     send_mail(
         subject="KYC Verification Approved",
         message=f"Hello {kyc.first_name},Your KYC has been approved.",
-        from_email="omotosoeniola191@gmail.com",
+        from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[kyc.email],
         fail_silently=True,
     )
@@ -140,7 +220,7 @@ def reject_kyc(request, kyc_id):
     send_mail(
         subject="KYC Verification Failed",
         message=f"Hello {kyc.first_name},Your KYC submission was rejected.",
-        from_email="omotosoeniola191@gmail.com",
+        from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[kyc.email],
         fail_silently=True,
     )
